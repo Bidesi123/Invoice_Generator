da195c05637b3e3c158aff65c642886ed1f44835
Bidesi123 <bidesh.sahoo.1@gmail.com>
2020-04-02 10:39:12 +0530

    [bidesi]Add.perform similar test for equality tc1.2-1.6

--- Quantity_Measurement_Test/FeetToInchesUnitTest.cs
+++ Quantity_Measurement_Test/FeetToInchesUnitTest.cs
@@ -14,5 +14,37 @@ namespace Quantity_Measurement_Test
             var actual = feet1.ConvertedFeetValue(feet2);
             Assert.IsTrue(actual);
         }
+
+        [Test]
+        public void NullFeet_ShouldReturn_False()
+        {
+            Feet ft1 = new Feet(11);
+            var actual = ft1.Equals(null);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void RefCheck_ForFeet_ShouldReturnFalse()
+        {
+            Feet ft1 = new Feet(11);
+            var actual = this.Equals(ft1);
+            Assert.IsFalse(actual);
+        }
+        [Test]
+        public void TypeCheck_ForFeet_ShouldReturnTrue()
+        {
+            Feet ft1 = new Feet(11);
+            Feet ft2 = new Feet(13);
+            var actual = ft1.Equals(ft2);
+            Assert.IsTrue(actual);
+        }
+        [Test]
+        public void GivenSimilar_FeetValues_WhileCheking_ShouldReturnTrue()
+        {
+            Feet ft1 = new Feet(11);
+            Feet ft2 = new Feet(11);
+            var actual = ft1.ConvertedFeetValue(ft2);
+            Assert.IsTrue(actual);
+        }
     }
 }
