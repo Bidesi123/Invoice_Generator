244ec6916d1976492f018be20fb5b8578edd7f8a
Bidesi123 <bidesh.sahoo.1@gmail.com>
2020-04-02 11:36:20 +0530

    [bidesi]implement FeetToInches Class

--- QuantityMeasurement/FeetToInches.cs
+++ QuantityMeasurement/FeetToInches.cs
@@ -4,8 +4,25 @@ using System.Text;
 
 namespace QuantityMeasurement
 {
-    class FeetToInches
+   public class FeetToInches
     {
+        public double feet;
+        public double inch;
+        public FeetToInches(Feet feet,Inches inch)
+        {
+            feet.feet = this.feet;
+            inch.inches = this.inch;
+        }
 
+        public bool ComparedFeetAndinchesValue(Feet feet,Inches inch)
+        {
+            if (feet.feet == 0 && feet.feet == inch.inches)
+                return true;
+            if (feet.feet == 1 && feet.feet == inch.inches)
+                return false;
+            if (feet.feet == 1 && inch.inches == 12)
+                return true;
+            return false;
+        }
     }
 }
